6bfa0338e4c8eb6c2b762650e74bc30f6b73f034
shubham <84614805+shubhamsharma0710@users.noreply.github.com>
2024-10-13 23:38:23 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,11 @@
+const accountId = 1234
+let accountEmail = "shubhamshrma@gmailcom"
+var accountPassward = "12133442"
+accountCity = "jaipur"
+
+
+accountEmail="sadfdsfggmhjk@gmail.com"
+accountPassward="8765432"
+accountCity = "aham"
+console.log(accountId);
+console.table([ accountEmail,accountPassward,accountCity]);
